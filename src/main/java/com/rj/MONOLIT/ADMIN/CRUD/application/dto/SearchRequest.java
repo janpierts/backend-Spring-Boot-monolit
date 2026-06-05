7c0d3429f3ba0d0f51b52df74c6e881945c0070b
Ruddy Janpierts Correa Grillo <30013868+janpierts@users.noreply.github.com>
2026-06-04 22:07:27 -0500

    change dir form && add controller Test

--- src/main/java/com/rj/MONOLIT/ADMIN/CRUD/application/dto/SearchRequest.java
+++
@@ -1,3 +0,0 @@
-package com.rj.MONOLIT.ADMIN.CRUD.application.dto;
-
-public record SearchRequest(String name, String email) {}
---
+++ src/main/java/com/rj/esqueleto/EsqueletoApplication.java
@@ -0,0 +1,13 @@
+package com.rj.esqueleto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsqueletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsqueletoApplication.class, args);
+	}
+
+}
