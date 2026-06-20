a8236c69ff0bb6fd63a562dcb245e35f48302c4b
janpierts <30013868+janpierts@users.noreply.github.com>
2026-06-20 17:50:49 -0500

    fix: clean code

--- src/main/java/com/rj/esqueleto/admin/crud/infrastructure/persistence/springdata/crudSpringDataRepository.java
+++ src/main/java/com/rj/esqueleto/admin/crud/infrastructure/persistence/springdata/crudSpringDataRepository.java
@@ -4,10 +4,9 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.rj.esqueleto.admin.crud.infrastructure.persistence.entity.CrudEntityJpa;
 
-@Repository
+//@Repository
 public interface crudSpringDataRepository extends JpaRepository<CrudEntityJpa, Long> {
     Optional<CrudEntityJpa> findByName(String name);
     List<CrudEntityJpa> findByNameIn(Collection<String> names);
